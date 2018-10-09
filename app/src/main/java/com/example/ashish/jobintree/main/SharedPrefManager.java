c3efbbe9d0d2777073b7b4d36319275d97eee1ee
shivamvk <shivam.vk529@gmail.com>
2018-10-09 22:29:21 +0530

    add shared pref manager

--- app/src/main/java/com/example/ashish/jobintree/main/SharedPrefManager.java
+++ app/src/main/java/com/example/ashish/jobintree/main/SharedPrefManager.java
@@ -1,4 +1,22 @@
 package com.example.ashish.jobintree.main;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SharedPrefManager {
+    private static SharedPrefManager sharedPrefManager;
+    private static Context context;
+
+    private static final String SHARED_PREF_NAME = "MySharedPref";
+
+    private SharedPrefManager(Context context) {
+        SharedPrefManager.context = context;
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context context) {
+        if (sharedPrefManager == null) {
+            sharedPrefManager = new SharedPrefManager(context);
+        }
+        return sharedPrefManager;
+    }
 }
